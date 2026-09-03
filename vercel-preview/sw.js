3c1e5a43639a479cb039715721ccaab5578eb3dc
darkstorm4u <142335479+darkstorm4u@users.noreply.github.com>
2026-09-03 13:01:41 -0500

    Add preview service worker

---
+++ vercel-preview/sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'lottery-hub-preview-v1';
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(Promise.all([
+  caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+  self.clients.claim()
+])));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
